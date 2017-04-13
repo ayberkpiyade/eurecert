d79871ec1f902d39200cc112427975d8fc71e703
Mustafa Fındık <mustafa.findik3@gmail.com>
2017-04-13 20:44:47 +0300

    ReferenceMethod Enum , PublicityMethod Enum and EducationType Create

---
+++ Eurecert/Models/PublicityMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eurecert.Models
+{
+    public enum PublicityMethod
+    {
+        Mailing = 1,
+        Presentation = 2,
+        Brochures = 3,
+        Other = 4
+    }
+}
---
+++ Eurecert/Models/ReferenceMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eurecert.Models
+{
+    public enum ReferenceMethod
+    {
+        Internet = 1,
+        Reference = 2,
+        Phone = 3,
+        Email = 4,
+        Other = 5
+
+    }
+}
